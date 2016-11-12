500bf95f81e8d81f5ddb22957f40020a116ef905
alastair.brown <alastair.brown@PBS47347L.home>
2016-11-12 09:51:15 +0000

    Refactor to reduce nesting

--- numerals/Arabify.java
+++ numerals/Arabify.java
@@ -4,46 +4,70 @@ import java.util.HashMap;
 public class Arabify
 {
 
-	HashMap<String, Integer> numeralMap = new HashMap<String, Integer>();
+	HashMap<String, Integer> numberMap = new HashMap<String, Integer>();
 
 	Arabify()
 	{
-		numeralMap.put("I",1);
-		numeralMap.put("V",5);
-		numeralMap.put("X",10);
-		numeralMap.put("L",50);
-		numeralMap.put("C",100);
-		numeralMap.put("D",500);
-		numeralMap.put("M",1000);
+		numberMap.put("I",1);
+		numberMap.put("V",5);
+		numberMap.put("X",10);
+		numberMap.put("L",50);
+		numberMap.put("C",100);
+		numberMap.put("D",500);
+		numberMap.put("M",1000);
 	}
-
-
-	public int toArab(String r)
+    
+    public int toArab(String r)
 	{
-
-			// Iterate through the string applying the rules
-			// Convert the current character and the next character to a value
-			// If the next value is smaller than the current value subtract the current from the next
-			// and sum the result to the running total
-			int n = 0;
-			for (int i = 0; i < r.length(); i++){
-				 String crd = r.substring(i,i+1); //Current Roman Digit
-				 int cav = numeralMap.get(crd);		//Current Arabic Value
-				 if (i < r.length() - 1){ // There's more in the "string"
-					 String nrd = r.substring((i+1),(i+2));	// Next Roman Digit
-					 int nav = numeralMap.get(nrd);		//Next Arabic Value
-					 if (nav > cav){
-						 n += (nav - cav);
-						 i++; // OMG that's dangerous
-					 }else{
-						 n += cav;
-					 }
-				}else{
-					n += cav;
-				}
-			}
-			return n;
+            int n = 0, i=0;
+            MutableBoolean isSubtractivePair = new MutableBoolean(false);
+			while (i < r.length()){
+                
+                String rp = getRomanPair(r,i);
+                int arabicValue = getArabicValueForRomanPair(rp, isSubtractivePair);
+                n += arabicValue;
+                if (isSubtractivePair.get() == true)
+                    i=i+2;
+                else
+                    i++;
+            }
+            return n;
 	}
+    
+    private String getRomanPair(String r, int i)
+    {
+        if(i < r.length() - 1)
+            return (r.substring(i,i+2));
+        else
+            return (r.substring(i,i+1));
+    }
+    
+    
+    private int getArabicValueForRomanPair(String rp, MutableBoolean s) 
+    {
+        
+        if (rp.length() == 1)
+            return numberMap.get(rp.substring(0,1));
+        
+        int thisArabicValue = numberMap.get(rp.substring(0,1));
+        int nextArabicValue = numberMap.get(rp.substring(1,2));
+        
+        //System.out.println(":thisArabicValue:" + thisArabicValue + "nextArabicValue:" + nextArabicValue );
+        if (nextArabicValue > thisArabicValue){
+            s.set(true);
+            return nextArabicValue - thisArabicValue;
+        } else {
+            s.set(false);
+            return thisArabicValue;
+        }
+    }
 
+
+    private class MutableBoolean{
+        boolean b;
+        MutableBoolean(boolean v){b=v;}
+        private void set(boolean v){b=v;}
+        private boolean get(){return(b);}
+    }
 
 }
--- numerals/TestArabify.java
+++ numerals/TestArabify.java
@@ -9,19 +9,19 @@ public class TestArabify
 
 	public static void main(String[] args)
 	{
-    test("I is 1", a.toArab("I"), 1);
-    test("V is 5", a.toArab("V"), 5);
-    test("X is 10", a.toArab("X"), 10);
-    test("L is 50", a.toArab("L"), 50);
-    test("C is 100", a.toArab("C"), 100);
-    test("D is 500", a.toArab("D"), 500);
-    test("M is 1000", a.toArab("M"), 1000);
-    test("IV is 4", a.toArab("IV"), 4);
-    test("VI is 6", a.toArab("VI"), 6);
-    test("VX is 5", a.toArab("VX"), 5);
-    test("MCMLXVIII is 1968", a.toArab("MCMLXVIII"), 1968);
-    test("MMMMXXXVII is 4037", a.toArab("MMMMXXXVII"), 4037);
-    test("MMMMXXVXII is 4027", a.toArab("MMMMXXVXII"), 4027);
+        test("I is 1", a.toArab("I"), 1);
+        test("V is 5", a.toArab("V"), 5);
+        test("X is 10", a.toArab("X"), 10);
+        test("L is 50", a.toArab("L"), 50);
+        test("C is 100", a.toArab("C"), 100);
+        test("D is 500", a.toArab("D"), 500);
+        test("M is 1000", a.toArab("M"), 1000);
+        test("IV is 4", a.toArab("IV"), 4);
+        test("VI is 6", a.toArab("VI"), 6);
+        test("VX is 5", a.toArab("VX"), 5);
+        test("MCMLXVIII is 1968", a.toArab("MCMLXVIII"), 1968);
+        test("MMMMXXXVII is 4037", a.toArab("MMMMXXXVII"), 4037);
+        test("MMMMXXVXII is 4027", a.toArab("MMMMXXVXII"), 4027);
 
 	}
 
