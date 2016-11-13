e7ec64f63e24fce311370d519828dd8845d2fee0
alastair.brown <alastair.brown@PBS47347L.local>
2016-11-13 08:39:14 +0000

    ToRoman works but doesnt observer subtractive rules

--- numerals/NumberMap.java
+++ numerals/NumberMap.java
@@ -1,10 +1,25 @@
 package numerals;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+
+
 
 public class NumberMap
 {
 
-	private HashMap<String, Integer> numberMap = new HashMap<String, Integer>();
+	private Map<String, Integer> numberMap = new HashMap<String, Integer>();
+    public static boolean ASC = true;
+    public static boolean DESC = false;
+
     
 	NumberMap()
 	{
@@ -18,6 +33,38 @@ public class NumberMap
 	}
     
     public int get(String r){return numberMap.get(r);}
+    public void sortDesc(){numberMap = sortByComparator(numberMap, DESC);}
+    public Map getMap(){return(numberMap);}
+    
+    
+    
+    private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap, final boolean order)
+    {
+        List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(unsortMap.entrySet());
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Entry<String, Integer>>()
+        {
+            public int compare(Entry<String, Integer> o1,
+                    Entry<String, Integer> o2)
+            {
+                if (order)
+                    return o1.getValue().compareTo(o2.getValue());
+                else
+                    return o2.getValue().compareTo(o1.getValue());
+
+            }
+        });
+
+        // Maintaining insertion order with the help of LinkedList
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Entry<String, Integer> entry : list)
+            sortedMap.put(entry.getKey(), entry.getValue());
+    
+        
+        return sortedMap;
+    }
+    
+    
     
 
 }
--- numerals/Romanify.java
+++ numerals/Romanify.java
@@ -1,20 +1,33 @@
 package numerals;
+import java.util.Set;
+import java.util.Map;
+import java.util.Iterator;
+
 
 public class Romanify
 {
 
 	NumberMap numberMap = new NumberMap();
-    MutableBoolean mb = new MutableBoolean(false);
     
     public String toRoman(int a)
 	{
-                
+            numberMap.sortDesc();
             String r = new String();
-            while (a > 0)
-            {
-                a = a/10;
-                r += "X";
-            }
+            Set set = numberMap.getMap().entrySet();
+            Iterator iterator = set.iterator();
+            while(iterator.hasNext()) {
+                Map.Entry me = (Map.Entry)iterator.next();
+                System.out.print(me.getKey() + ": ");
+                System.out.println(me.getValue());
+                while (a >= (Integer) me.getValue())
+                {
+                    a = a - (Integer) me.getValue();
+                    r += me.getKey();
+                    System.out.println("a:" + a + " r:" + r);
+                }
+            }    
+            
+
             return (r);
 	}
 
